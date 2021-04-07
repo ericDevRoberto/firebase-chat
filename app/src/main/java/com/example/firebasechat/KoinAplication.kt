eb981d97931c46c8d43d3573e330adbc8991e688
Eric <ericsilva328@gmail.com>
2021-04-07 10:45:07 -0300

    koin added

---
+++ app/src/main/java/com/example/firebasechat/KoinAplication.kt
@@ -0,0 +1,20 @@
+package com.example.firebasechat
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class KoinAplication :Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@KoinAplication)
+
+            modules(listOf(
+
+            ))
+        }
+    }
+}
